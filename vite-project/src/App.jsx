4f6a75c2d50929fb64faf939099a1491e03e2b23
Mykola <nicholas.rabotnikov@gmail.com>
2025-03-22 15:28:01 +0100

    Boba Big Changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Boba from '../public/boba.jpg'
 import './App.css'
 
 function App() {
@@ -9,12 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={Boba} className='boba' alt='boba' />
       </div>
       <h1>Bobka Sosal penis</h1>
       <div className="card">
---
+++ vite-project/src/BobaComponent.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function BobaComponent(){
+<image src="../public/boba.jpg"></image>
+}
+export default BobaComponent
