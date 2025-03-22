1e6fd7d6c2c2077f91013a0c940b5e6fb760fb6e
Mykola <nicholas.rabotnikov@gmail.com>
2025-03-22 01:34:37 +0100

    Small config changes

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,5 @@ import { copyFileSync } from "fs";
 export default defineConfig({
   plugins: [react()],
   base: "/jaloqa.github.io/",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      plugins: [
-        {
-          name: "copy-nojekyll",
-          buildEnd() {
-            copyFileSync("public/.nojekyll", "dist/.nojekyll");
-          },
-        },
-      ],
-    },
-  },
+  
 });
