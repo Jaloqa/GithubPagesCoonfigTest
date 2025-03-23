d9ccd9c6c89863b49c15e6906d0ce96270043555
Mykola <nicholas.rabotnikov@gmail.com>
2025-03-23 23:40:28 +0100

    Configuration changes. Adding new UI library

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,28 +1,10 @@
-import { useState } from 'react'
-import Boba from '../public/boba.jpg'
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <img src={Boba} className='boba' alt='boba' />
-      </div>
-      <h1>Bobka Sosal penis</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<Header/>
   )
 }
 
